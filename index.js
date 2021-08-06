692ff22244a09bb852734e348225573a541814c7
BaseerIqbal <81772380+BaseerIqbal@users.noreply.github.com>
2021-08-06 16:34:01 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,43 @@
+const texts = document.querySelector('.texts');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result', (e) =>{
+
+  const text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+
+    p.innerText = text;
+    texts.appendChild(p);
+
+    if(e.results[0].isFinal){
+      if(text.includes('hello')){
+        p = document.createElement('p');
+        p.classList.add('reply');
+        p.innerText = 'Hi there! how may I help you ?';
+        texts.appendChild(p);
+      }
+      if(text.includes('goodbye')){
+        p = document.createElement('p');
+        p.classList.add('reply');
+        p.innerHTML = '<img src="images/wave.png"> <br> Have a good day!';
+        texts.appendChild(p);
+      }
+      p = document.createElement('p');
+    }
+  console.log(text);
+});
+
+recognition.addEventListener('end', ()=>{
+  recognition.start();
+});
+
+recognition.start();
